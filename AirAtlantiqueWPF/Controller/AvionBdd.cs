d7f51a9f390261364adb2d23ef9d490f36a01eb3
Unknown <dylan.gogeta@gmail.com>
2018-12-05 16:33:47 +0100

    Uptaded
    Controller avion, gestion bdd pour avions, objet Avion

---
+++ AirAtlantiqueWPF/Controller/AvionBdd.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace AirAtlantiqueWPF.Controller
+{
+    class AvionBdd
+    {
+        private static MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+        private int port;
+
+        public bool Initialize()
+        {
+            server = "localhost";
+            database = "airatlantiquecsharp";
+            uid = "dev";
+            password = "azerty123";
+            port = 3307;
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "Port=" + port + ";" + "DATABASE=" +
+                               database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+            connection.Open();
+            return true;
+        }
+        public void SelectAvion(ObservableCollection<Avion> l)
+        {
+            string query = "SELECT * FROM avions";
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Avion a = new Avion(reader.GetInt32(0), reader.GetString(1), reader.GetInt32(2), reader.GetInt32(3), reader.GetInt32(4), reader.GetInt32(5), reader.GetInt32(6), reader.GetInt32(7), reader.GetInt32(8));
+                l.Add(a);
+            }
+            reader.Close();
+        }
+    }
+}
---
+++ AirAtlantiqueWPF/Controller/AvionsController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media.Media3D;
+//using System.Windows.Forms;
+using MenuItem = System.Windows.Forms.MenuItem;
+using MainMenu = System.Windows.Forms.MainMenu;
+
+namespace AirAtlantiqueWPF.Controller
+{
+    public class Avion : INotifyPropertyChanged
+    {
+        private int idAvion;
+        private string ModeleAvion;
+        private int Capacite;
+        private int EtatAvion;
+        private int NbrPremium;
+        private int NbrBusiness;
+        private int NbrEco;
+        private int idStock;
+        private int Type;
+
+        public int idAvionProperty
+        {
+            get { return idAvion; }
+        }
+
+        public string ModelAvionProperty
+        {
+            get { return ModeleAvion; }
+            set
+            {
+                this.ModeleAvion = value;
+            }
+        }
+        public int CapaciteProperty
+        {
+            get { return Capacite; }
+            set
+            {
+                this.Capacite = value;
+            }
+        }
+        public int EtatAvionProperty
+        {
+            get { return EtatAvion; }
+            set
+            {
+                this.EtatAvion = value;
+            }
+        }
+        public int NbrPremiumProperty
+        {
+            get { return NbrPremium; }
+            set
+            {
+                this.NbrPremium = value;
+            }
+        }
+        public int NbrBusinessProperty
+        {
+            get { return NbrBusiness; }
+            set
+            {
+                this.NbrBusiness = value;
+            }
+        }
+        public int NbrEcoProperty
+        {
+            get { return NbrEco; }
+            set
+            {
+                this.NbrEco = value;
+            }
+        }
+        public int idStockProperty
+        {
+            get { return idStock; }
+            set
+            {
+                this.idStock = value;
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+}
